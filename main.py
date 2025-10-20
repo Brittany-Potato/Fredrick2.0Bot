515edff77177d326fb49ab476c67529c2f60d837
brittany-potato <brittanycc@missionreadyhq.com>
2025-10-20 13:31:11 +1300

    Created main.py base and added __init__ files in appropriate folders

---
+++ main.py
@@ -0,0 +1,18 @@
+# main.py
+
+import asyncio
+from discord.bot import bot as discord_bot
+from twitch.bot import bot as twitch_bot
+from config import DISCORD_TOKEN
+
+async def main():
+    await asyncio.gather(
+        discord_bot.start(DISCORD_TOKEN),
+        twitch_bot.start()
+    )
+    
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Bots are shutting down.")
